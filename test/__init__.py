7bee103742d9eccd5f6cbb96fecef9d2dd6e4884
skullquake <ockert8080@gmail.com>
2025-08-25 13:47:34 +0200

    .
    Signed-off-by: skullquake <ockert8080@gmail.com>

--- test/__init__.py
+++ test/__init__.py
@@ -3,7 +3,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     resp = {
         "status": "success",
-        "message": "test1"
+        "message": "test2"
     }
     return func.HttpResponse(
         json.dumps(resp),
